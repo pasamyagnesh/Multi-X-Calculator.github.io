98a1925ba2e32ef3b2d7a8ee462506706e3c0aea
yagnesh2004 <93670699+yagnesh2004@users.noreply.github.com>
2023-02-09 15:25:02 +0530

    js file is modified

--- calculator.js
+++ calculator.js
@@ -5,3 +5,52 @@ const allClearButton=document.querySelectorAll('[data-all-clear]')
 const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
 const presentOperandTextElement = document.querySelectorAll('[data-present-operand]')
 
+
+
+class calculator {
+    constructor(presentOperandTextElement,presentOperandTextElement)
+    this.presentOperandTextElement =previousOperandTextElement
+    this.presentOperandTextElement = presentOperandTextElement
+}
+
+clear(){
+    this.presentOperand  = ''
+    this.previousOperand =''
+    this.operation = undefined
+
+}
+
+delete_function(){
+
+}
+chooseOperayion(operation)
+{
+
+}
+appendNumber()
+{
+
+}
+compute(){
+
+}
+updateDisplay(){
+
+}
+
+
+
+
+
+
+
+
+const calculator =new calculator(presentOperandTextElement,presentOperandTextElement)
+numberButton.forEach(button =>{
+    
+})
+
+
+
+
+
